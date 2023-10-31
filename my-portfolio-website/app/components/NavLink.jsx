68daf2f9f4fcc1bc3a1ff99006b097cd6a646a88
Yu Hamada <yuham1014@gmail.com>
2023-10-31 11:34:07 -0700

    fixed the color of header bar

--- my-portfolio-website/app/components/NavLink.jsx
+++ my-portfolio-website/app/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link 
       href={href} 
-      className="text-gray-300 hover:bg-gradient-to-br from-indigo-800 via-blue-500 to-cyan-300 hover:text-white block px-4 py-3 rounded-md text-lg font-medium"
+      className="text-gray-300 hover:bg-gradient-to-br from-indigo-800 via-blue-500 to-cyan-300 hover:text-white block px-5 py-4 rounded-md text-lg font-medium"
     >
       {title}
     </Link>
--- my-portfolio-website/app/page.js
+++ my-portfolio-website/app/page.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+    {/* // <main className="flex min-h-screen flex-col bg-red-500"> */}
       <Navbar />
       <div className="container mx-auto mt-20">
         <HeroSection />
